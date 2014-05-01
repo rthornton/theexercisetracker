b2b9ff74d4824a923ae4afd0c8193f146065fafe
Rob Thornton <robert.thornton@gmail.com>
2014-05-01 11:22:14 -0400

    Adding sorting by year & week.  Week currently sorting by aligned day of week which is not what is desired.

--- src/main/java/com/theexercisetracker/service/OverallStatisticsService.java
+++ src/main/java/com/theexercisetracker/service/OverallStatisticsService.java
@@ -2,7 +2,11 @@ package com.theexercisetracker.service;
 
 import com.theexercisetracker.persistence.model.Activity;
 
-import java.util.List;
+import java.time.Month;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+import java.util.*;
 
 public class OverallStatisticsService {
     public long calculateTotalDistance(List<Activity> activities) {
@@ -20,5 +24,47 @@ public class OverallStatisticsService {
             total += a.getTotalCaloriesBurned();
         }
         return total;
+    }
+
+    /**
+     * Group activities by year/month
+     * @return Map of week --> List of Activities
+     * @param activities
+     */
+    public List<List<Activity>> groupActivitiesByWeek(List<Activity> activities) {
+        Map<Integer, List<Activity>> activitiesByYear = groupActivitiesByYear(activities);
+
+        List<List<Activity>> activitiesByWeek = new ArrayList<>();
+        for (Integer year : activitiesByYear.keySet()) {
+            Map<Integer, List<Activity>> activitiesByWeekOfYear = new TreeMap<>();
+
+            for (Activity activity : activitiesByYear.get(year)) {
+                ZonedDateTime startTime = activity.getStartTime();
+                if (!activitiesByWeekOfYear.containsKey(startTime.get(ChronoField.ALIGNED_WEEK_OF_YEAR))) {
+                    activitiesByWeekOfYear.put(startTime.get(ChronoField.ALIGNED_WEEK_OF_YEAR), new ArrayList<>());
+                }
+
+                activitiesByWeekOfYear.get(startTime.get(ChronoField.ALIGNED_WEEK_OF_YEAR)).add(activity);
+            }
+
+            for(Integer week : activitiesByWeekOfYear.keySet()) {
+                activitiesByWeek.add(activitiesByWeekOfYear.get(week));
+            }
+        }
+
+        return activitiesByWeek;
+    }
+
+    public Map<Integer, List<Activity>> groupActivitiesByYear(List<Activity> activities) {
+        Map<Integer, List<Activity>> activitiesByYear = new TreeMap<>();
+        for (Activity activity : activities) {
+            ZonedDateTime startTime = activity.getStartTime();
+            if (!activitiesByYear.containsKey(startTime.getYear())) {
+                activitiesByYear.put(startTime.getYear(), new ArrayList<>());
+            }
+
+            activitiesByYear.get(startTime.getYear()).add(activity);
+        }
+        return activitiesByYear;
     }
 }
--- src/test/java/com/theexercisetracker/service/OverallStatisticsServiceTest.java
+++ src/test/java/com/theexercisetracker/service/OverallStatisticsServiceTest.java
@@ -5,8 +5,11 @@ import com.theexercisetracker.persistence.model.Activity;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class OverallStatisticsServiceTest {
 
@@ -22,17 +25,86 @@ public class OverallStatisticsServiceTest {
         Assert.assertEquals(2000, new OverallStatisticsService().calculateTotalCalories(activities));
     }
 
-    /**
-     * Sort by date
-     * Want JPA query to return sorted by date
-     *
-     */
+    @Test
+    public void groupActivitiesByYear() {
+        List<Activity> activities = generateWeeklyActivities();
+        Map<Integer, List<Activity>> groupActivitiesByYear = new OverallStatisticsService().groupActivitiesByYear(activities);
+        Assert.assertEquals(2, groupActivitiesByYear.keySet().size());
+        Assert.assertEquals(1, groupActivitiesByYear.get(2013).size());
+        Assert.assertEquals(new Integer(5), groupActivitiesByYear.get(2013).get(0).getId());
+        Assert.assertEquals(5, groupActivitiesByYear.get(2014).size());
+    }
 
     @Test
-    public void calculateDistanceByWeek() {
-        List<Activity> activities = generateActivities();
-//        Map<>
-        Assert.assertEquals(2000, new OverallStatisticsService().calculateTotalCalories(activities));
+    public void groupActivitiesByWeek() {
+        List<Activity> activities = generateWeeklyActivities();
+        List<List<Activity>> activitiesByWeek = new OverallStatisticsService().groupActivitiesByWeek(activities);
+        Assert.assertEquals(4, activitiesByWeek.size());
+        Assert.assertEquals(1, activitiesByWeek.get(0).size());
+        Assert.assertEquals(new Integer(5), activitiesByWeek.get(0).get(0).getId());
+        Assert.assertEquals(2, activitiesByWeek.get(1).size());
+        Assert.assertEquals(1, activitiesByWeek.get(2).size());
+        Assert.assertEquals(2, activitiesByWeek.get(3).size());
+    }
+
+    /*
+      W1:  Multiple per week, crossing year & month boundary
+        - A1
+        - A4
+        - A5
+      W2:  Single
+        - A2
+      W3:  Multiple per week, no boundary crossing
+        - A3
+        - A6
+     */
+    private List<Activity> generateWeeklyActivities() {
+        List<Activity> activities = new ArrayList<>();
+
+        Activity a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2014, 1, 1, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(1000);
+        a.setTotalCaloriesBurned(500);
+        a.setId(1);
+        activities.add(a);
+
+        a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2014, 1, 5, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(2000);
+        a.setTotalCaloriesBurned(1500);
+        a.setId(2);
+        activities.add(a);
+
+        a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2014, 2, 3, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(2000);
+        a.setTotalCaloriesBurned(1500);
+        a.setId(3);
+        activities.add(a);
+
+        a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2014, 1, 3, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(2000);
+        a.setTotalCaloriesBurned(1500);
+        a.setId(4);
+        activities.add(a);
+
+        a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2013, 12, 31, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(2000);
+        a.setTotalCaloriesBurned(1500);
+        a.setId(5);
+        activities.add(a);
+
+        a = new Activity();
+        a.setStartTime(ZonedDateTime.of(2014, 2, 5, 1, 1, 1, 1, ZoneId.systemDefault()));
+        a.setDistanceInMeters(2000);
+        a.setTotalCaloriesBurned(1500);
+        a.setId(6);
+        activities.add(a);
+
+
+        return activities;
     }
 
     private List<Activity> generateActivities() {
